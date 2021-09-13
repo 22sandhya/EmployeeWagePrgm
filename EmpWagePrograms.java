11431405b11d7fbac51b32ade500184a96369131
Admin <sandhyabagale10@gmail.com>
2021-09-13 15:08:40 +0530

    we are added daily wage program

--- EmpWagePrograms.java
+++ EmpWagePrograms.java
@@ -7,40 +7,22 @@ public class EmpWagePrograms {
 	public static void main(String[] args) {
 
 		System.out.println("welcome to emloyee wage computation program");
-		final int IS_PART_TIME = 1;
-		final int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 100;
-		int NO_OF_WORKING_DAYS = 20;
-		int MAX_HRS_IN_THE_MONTH = 10;
-		{
-			int empHrs = 0;
-			int totalEmpHrs = 0;
-			int totalWorkingDays = 0;
-			while (totalEmpHrs <= MAX_HRS_IN_THE_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
-				totalWorkingDays++;
-				Random random = new Random();
-				int empCheck = random.nextInt(3);
-				switch (empCheck) {
-				case IS_PART_TIME:
-					empHrs = 8;
-					break;
-
-				case IS_FULL_TIME:
-					empHrs = 12;
-					break;
-
-				default:
-					empHrs = 0;
-					break;
-
-				}
-
-				totalEmpHrs += empHrs;
-				System.out.println("Days#" + totalWorkingDays + "EmpHrs :" + empHrs);
-			}
-			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Total Emp Wage:" + totalEmpWage);
-
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+	
+		int empHrs = 0;
+		int empWage = 0;
+		Random random = new Random();
+		int empCheck = random.nextInt(3);
+		if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		// else
+		// empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 		}
 	}
-}
+
