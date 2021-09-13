c6786d3f032812694d09cc4e1a6141269057d587
Admin <sandhyabagale10@gmail.com>
2021-09-13 14:51:52 +0530

    we are added SwitchCase program here

--- EmpWagePrograms.java
+++ EmpWagePrograms.java
@@ -7,21 +7,29 @@ public class EmpWagePrograms {
 	public static void main(String[] args) {
 
 		System.out.println("welcome to emloyee wage computation program");
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-
 		int empHrs = 0;
 		int empWage = 0;
 		Random random = new Random();
 		int empCheck = random.nextInt(3);
-		if (empCheck == IS_PART_TIME)
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 8;
-		else if (empCheck == IS_FULL_TIME)
-			empHrs = 12;
+			System.out.println("Employee is present part time");
+			break;
 
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+		case IS_FULL_TIME:
+			empHrs = 8;
+			System.out.println("Employee is present full time");
+			break;
 
+		default:
+			empHrs = 0;
+			System.out.println("Employee is absent");
+			break;
+
+		}
 	}
 }
