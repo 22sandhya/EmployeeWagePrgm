4214d8a105c9aef9253ac15616d0542bac802054
Admin <sandhyabagale10@gmail.com>
2021-09-13 14:26:00 +0530

    we are added PresentAbsent program

--- EmpWagePrograms.java
+++ EmpWagePrograms.java
@@ -1,11 +1,19 @@
 package com.bz.emp;
 
+import java.util.Random;
+
 public class EmpWagePrograms {
 
 	public static void main(String[] args) {
 
 		System.out.println("welcome to emloyee wage computation program");
-
+		int IS_FULL_TIME = 1;
+		Random random = new Random();
+		int empCheck = random.nextInt(3);
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
