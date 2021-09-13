533feb321475637b87dee2d154f33823f72a9e97
Admin <sandhyabagale10@gmail.com>
2021-09-13 14:13:09 +0530

    welcome message here

---
+++ EmpWagePrograms.java
@@ -0,0 +1,11 @@
+package com.bz.emp;
+
+public class EmpWagePrograms {
+
+	public static void main(String[] args) {
+
+		System.out.println("welcome to emloyee wage computation program");
+
+	}
+
+}
